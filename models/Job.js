0d92e00f8c6e19c460e4fa98a25026f7f585a467
Copheles <xixxykitkit@gmail.com>
2023-02-27 04:20:12 +0630

    Mock Data to analyze

--- models/Job.js
+++ models/Job.js
@@ -5,7 +5,7 @@ const JobSchema = new mongoose.Schema({
   company: {
     type: String,
     required: [true, 'Please provide company'],
-    maxlength: 20,
+    maxlength: 50,
   },
   position: {
     type: String,
