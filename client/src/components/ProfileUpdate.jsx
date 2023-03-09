e1997365c48d67c4bcafefe1ec24c9034b5f19ab
Copheles <xixxykitkit@gmail.com>
2023-03-09 18:10:48 +0630

    fixing pop up bug

--- client/src/components/ProfileUpdate.jsx
+++ client/src/components/ProfileUpdate.jsx
@@ -52,71 +52,77 @@ const ProfileUpdate = () => {
   };
 
   return (
-    <Flex
-      justifyContent={{ base: 'center', lg: 'flex-start'}}
-      flexDir={{ base: "column", lg: "row" }}
-      py={{ base: 3, lg: 10}}
-      maxW='5xl'
-    >
+    <>
       {isShowAlert && <AlertPopUp {...alertDetails} />}
-
       <Flex
-        flexDirection="column"
-        px={{ base: 10, lg: 2 }}
-        w={{ base: "100%", lg: "30%" }}
-        mb={{ base: 10, lg: 0 }}
+        justifyContent={{ base: "center", lg: "flex-start" }}
+        flexDir={{ base: "column", lg: "row" }}
+        py={{ base: 3, lg: 10 }}
+        maxW="5xl"
       >
-        <Box textAlign="center" mb={5}>
-          <Avatar size="lg" alignItems="center" mb={5} />
-          <Heading textAlign="center" fontSize={{ base: "24px" }} mb={3}>
-            {name}
-          </Heading>
-          <Heading
-            fontSize={{ base: "14px" }}
-            textAlign="center"
-            color={mode("gray.500", "gray.500")}
-          >
-            Joined on {date}
-          </Heading>
-        </Box>
-      </Flex>
-      <SimpleGrid columns={{ base: 1, md: 2 }} gap={3} mb={{ base: 5, lg: 0}} flex={1}>
-        <FormRow
-          type="text"
-          name="name"
-          icon={<FaUserAlt />}
-          value={name}
-          handleChange={handleChange}
-          labelText="Name"
-        />
-        <FormRow
-          type="email"
-          name="email"
-          value={email}
-          handleChange={handleChange}
-          labelText="Email"
-          icon={<EmailIcon />}
-        />
-        <FormRow
-          type="text"
-          name="location"
-          value={location}
-          handleChange={handleChange}
-          labelText="Location"
-          icon={<FaLocationArrow />}
-        />
-
-        <Button
-          w="full"
-          colorScheme="red"
-          mt={{ base: 0, md: 8 }}
-          mb={5}
-          onClick={handleUpdate}
+        <Flex
+          flexDirection="column"
+          px={{ base: 10, lg: 2 }}
+          w={{ base: "100%", lg: "30%" }}
+          mb={{ base: 10, lg: 0 }}
         >
-          Save Changes
-        </Button>
-      </SimpleGrid>
-    </Flex>
+          <Box textAlign="center" mb={5}>
+            <Avatar size="lg" alignItems="center" mb={5} />
+            <Heading textAlign="center" fontSize={{ base: "24px" }} mb={3}>
+              {name}
+            </Heading>
+            <Heading
+              fontSize={{ base: "14px" }}
+              textAlign="center"
+              color={mode("gray.500", "gray.500")}
+            >
+              Joined on {date}
+            </Heading>
+          </Box>
+        </Flex>
+        <SimpleGrid
+          columns={{ base: 1, md: 2 }}
+          gap={3}
+          mb={{ base: 5, lg: 0 }}
+          flex={1}
+        >
+          <FormRow
+            type="text"
+            name="name"
+            icon={<FaUserAlt />}
+            value={name}
+            handleChange={handleChange}
+            labelText="Name"
+          />
+          <FormRow
+            type="email"
+            name="email"
+            value={email}
+            handleChange={handleChange}
+            labelText="Email"
+            icon={<EmailIcon />}
+          />
+          <FormRow
+            type="text"
+            name="location"
+            value={location}
+            handleChange={handleChange}
+            labelText="Location"
+            icon={<FaLocationArrow />}
+          />
+
+          <Button
+            w="full"
+            colorScheme="red"
+            mt={{ base: 0, md: 8 }}
+            mb={5}
+            onClick={handleUpdate}
+          >
+            Save Changes
+          </Button>
+        </SimpleGrid>
+      </Flex>
+    </>
   );
 };
 
