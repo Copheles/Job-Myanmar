cab4e44c5c40af23ed1aee1f6aefbb3080e7c5a0
Copheles <xixxykitkit@gmail.com>
2024-05-08 00:53:10 +0630

    checking

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ app.use(errorHandlerMiddleware)
 const __dirname = path.resolve();
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  console.log('production true');
+  app.use(express.static(path.join(__dirname, '/client/build')))
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
