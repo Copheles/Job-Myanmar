762fe1883ac14e993dab5703a73040928cbc4adf
Copheles <xixxykitkit@gmail.com>
2025-02-04 12:52:12 +0630

    fix production error

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -41,7 +41,7 @@ app.use('/api/v1/comments', authenticatedUser, commentRouter)
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
 }
 
 
--- dist/server.js
+++ dist/server.js
@@ -30,7 +30,7 @@ app.use('/api/v1/jobs', authenticatedUser, jobsRouter);
 app.use('/api/v1/comments', authenticatedUser, commentRouter);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
 }
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
